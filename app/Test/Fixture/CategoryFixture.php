74cc3e11f9bd8d37d9818d5d79ec720339a8a471
Naoto Mawatari <github.mawatari@gmail.com>
2012-10-08 19:18:19 +0900

    bake all Category

---
+++ app/Test/Fixture/CategoryFixture.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * CategoryFixture
+ *
+ */
+class CategoryFixture extends CakeTestFixture {
+
+/**
+ * Fields
+ *
+ * @var array
+ */
+	public $fields = array(
+		'id' => array('type' => 'integer', 'null' => false, 'default' => null, 'key' => 'primary'),
+		'name' => array('type' => 'string', 'null' => false, 'default' => null, 'collate' => 'utf8_unicode_ci', 'charset' => 'utf8'),
+		'created' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'modified' => array('type' => 'datetime', 'null' => false, 'default' => null),
+		'indexes' => array(
+			'PRIMARY' => array('column' => 'id', 'unique' => 1)
+		),
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+	);
+
+/**
+ * Records
+ *
+ * @var array
+ */
+	public $records = array(
+		array(
+			'id' => 1,
+			'name' => 'Lorem ipsum dolor sit amet',
+			'created' => '2012-10-08 19:17:54',
+			'modified' => '2012-10-08 19:17:54'
+		),
+	);
+
+}
