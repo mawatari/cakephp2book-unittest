b654b4fd7ecbec82080eec8d1a514366a0adfe36
Naoto Mawatari <github.mawatari@gmail.com>
2012-10-08 21:45:08 +0900

    Topic Modelのテストを作成

--- app/Test/Case/Model/TopicTest.php
+++ app/Test/Case/Model/TopicTest.php
@@ -39,4 +39,22 @@ class TopicTest extends CakeTestCase {
 		parent::tearDown();
 	}
 
+	public function testタイトルは必須入力である()
+	{
+		$this->Topic->create(array('Topic' => array('title' => '')));
+		$this->assertFalse($this->Topic->validates());
+		$this->assertArrayHasKey('title', $this->Topic->validationErrors);
+	}
+
+	public function testカテゴリ1の最新5件が取得できること()
+	{
+		$latests = $this->Topic->getLatest();
+		$this->assertCount(5, $latests);
+		$this->assertEquals('CG入門', $latests[0]['Topic']['title']);
+		$this->assertEquals('はじめてのWindows', $latests[1]['Topic']['title']);
+		$this->assertEquals('はじめてのPHP', $latests[2]['Topic']['title']);
+		$this->assertEquals('格好良いスマートフォン', $latests[3]['Topic']['title']);
+		$this->assertEquals('新しい携帯電話', $latests[4]['Topic']['title']);
+	}
+
 }
